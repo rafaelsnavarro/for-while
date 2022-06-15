f2bb6117b351e76b5d58516a353e4b85f58d2ec8
Rafael S. Navarro <rafaelsnavarro26@gmail.com>
2022-06-14 23:33:41 -0300

    exercicio 3 do módulo 2 do vai na web

---
+++ script.js
@@ -0,0 +1,23 @@
+/*1 - Criar um loop que conte de 1 até 10 usando FOR */
+
+for (let cont = 1; cont <=10; cont +=1){
+    console.log(cont)
+}
+/*2 - Criar um loop que conte de 10 até 1 usando WHILE*/
+let contador = 10
+while(contador >= 1){
+    console.log(contador)
+    contador -=1;
+}
+/*3 - Criar um loop que conte todos os números ímpares de 1 até 100 */
+let num = 1
+while(100 >= num){
+    console.log(num);
+    num += 2;
+}
+/*4- Criar um loop que conte todos os números pares de 0 a 100 */
+let numero = 0
+ while (100 >= numero) {
+   console.log (numero);
+    numero += 2;
+ }
